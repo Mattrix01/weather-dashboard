a73a134db4f3f8301b759e31e8f59d4988e3b4e0
Mattrix01 <emjay.knight@gmail.com>
2022-02-18 15:14:18 +0000

    Wetaher displaying correctly on main card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,67 @@
-const APIkey = "7e6847b6027bd0af581a71ea3d6caf97";
-var city = "london";
-
-var getWeather = function () {
-  var cityURL =
-    "https://api.openweathermap.org/geo/1.0/direct?q=" +
-    city +
-    "&limit=5&appid=" +
-    APIkey;
-  fetch(cityURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
+let weather = {
+  apiKey: "7e6847b6027bd0af581a71ea3d6caf97",
+  // fetching weather
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  // Append function
+  displayWeather: function (data) {
+    // extracting name from this object and making it into a variable
+    const { name } = data;
+    // extracting icon and description from data, weather object
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    // speed taken out of that object data and made into a variable
+    const { speed } = data.wind;
+    //
+    console.log(name, icon, description, temp, humidity, speed);
+    // appending to document classes set up
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temperature").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+  },
+  // function to get content of search bar
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
 };
+// selectors, on search-box button to search for the search content in weather variable
+document
+  .querySelector(".search-box button")
+  .addEventListener("click", function () {
+    weather.search();
+  });
+// selector for pressing enter key, taking in event to the parameter.
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    // if key is enter, do weather search function
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+// when load page default to london
+weather.fetchWeather("London");
 
-getWeather();
 // original plan
 // var searchBoxEl = document.querySelector("#searchBox");
 // var searchBtnEl = document.querySelector("#searchBtnEl");
@@ -31,15 +76,15 @@ getWeather();
 //this data we use to render onto the html sections 'current' and 'forecast'
 // }
 
-function buttonClickHandler() {
-  // get what value the user has tped in search box
-  // .trim etc remove uneeded spaces
-  //first - add the search to local storage
-  //second - use city name to get lat and lon from the api  - on the first fetch request we concatenate the url with the city
-  //destrcuture the response and get the latitude and loningitude for the city response.coord.lat, response.coord.lon pass them into getWeather function
-  // var lat = for example
-  //use lat and lon and pass them into another function which will get the current and future weather
-}
+// function buttonClickHandler() {
+// get what value the user has tped in search box
+// .trim etc remove uneeded spaces
+//first - add the search to local storage
+//second - use city name to get lat and lon from the api  - on the first fetch request we concatenate the url with the city
+//destrcuture the response and get the latitude and loningitude for the city response.coord.lat, response.coord.lon pass them into getWeather function
+// var lat = for example
+//use lat and lon and pass them into another function which will get the current and future weather
+// };
 
 // searchBtnEl.addEventListener("click", buttonClickHandler);
 
